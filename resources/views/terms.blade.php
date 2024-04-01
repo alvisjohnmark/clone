a20ea2796886b4d2d020107ec13be0fd971edd9e
John Mark Alvis <100738110+alvisjohnmark@users.noreply.github.com>
2024-04-01 21:50:19 +0800

    set routes

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <h1>Terms</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quas corrupti quam praesentium ducimus laudantium
+        voluptates tempore at quis nisi.</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/term', [DashboardController::class, 'terms']);
